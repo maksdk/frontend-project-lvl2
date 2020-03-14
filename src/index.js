f9cd00dd414df07c3e9417a915fa26652b346df1
maksdk <havryshchenko@gmail.com>
2020-03-14 10:44:42 +0200

    added outputing with commander

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // @ts-check
+import { program } from 'commander';
+// @ts-ignore
+import packageConfig from '../package.json';
+
+const { version } = packageConfig;
+
+program
+  .version(version)
+  .description('Compares two configuration files and shows a difference.');
 
 const run = () => {
-  console.log('run');
+  program.parse(process.argv);
 };
 
 export default run;
