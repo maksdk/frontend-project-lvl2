126726b12f2e324f8fe7f28fed6699d4aa7409e5
maksdk <havryshchenko@gmail.com>
2020-03-14 11:03:38 +0200

    added arguments / actions / new options

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ const { version } = packageConfig;
 
 program
   .version(version)
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log('firstConfig: ', firstConfig);
+    console.log('secondConfig: ', secondConfig);
+  });
 
 const run = () => {
   program.parse(process.argv);
