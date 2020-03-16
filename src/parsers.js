58fdc568274a4f0092e721b3fac5d6a9cb726089
maksdk <havryshchenko@gmail.com>
2020-03-16 22:34:48 +0200

    added error

--- src/parsers.js
+++ src/parsers.js
@@ -8,7 +8,7 @@ const parsers = {
 
 export default (config, type) => {
   if (!parsers[type]) {
-    throw new Error('Such parser type is not registered!');
+    throw new Error(`Such parser type: ${type} is not registered!`);
   }
 
   const parse = parsers[type];
