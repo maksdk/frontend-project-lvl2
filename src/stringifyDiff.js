98ea25f8cc89126876df686035dc0c15d5103e0b
maksdk <havryshchenko@gmail.com>
2020-03-20 09:00:13 +0200

    added diff stringified function

--- src/stringifyDiff.js
+++ src/stringifyDiff.js
@@ -4,27 +4,62 @@ const mapProperiesStates = {
   new: (key, value) => [`+ ${key}: ${value}`],
   modified: (key, value, prevVelue) => [`+ ${key}: ${value}`, `- ${key}: ${prevVelue}`],
   deleted: (key, value) => [`- ${key}: ${value}`],
-  old: (key, value) => [`  ${key}: ${value}`],
+  unchanged: (key, value) => [`  ${key}: ${value}`],
 };
 
-const stringify = (differences) => {
+const stringifySimpleDiff = (diff, depth, stringifyFunc) => {
+  const {
+    state = 'unchanged',
+    key,
+    value,
+    children,
+  } = diff;
+
+  if (Array.isArray(children)) {
+    return mapProperiesStates[state](key, stringifyFunc(children, depth));
+  }
+
+  return mapProperiesStates[state](key, value);
+};
+
+const stringifyModifiedDiff = (diff, depth, stringifyFunc) => {
+  const {
+    key,
+    value,
+    children,
+    oldValue,
+  } = diff;
+
+  if (Array.isArray(children) && Array.isArray(oldValue)) {
+    return mapProperiesStates
+      .modified(key, stringifyFunc(children, depth), stringifyFunc(oldValue, depth));
+  }
+
+  if (Array.isArray(children)) {
+    return mapProperiesStates.modified(key, stringifyFunc(children, depth), oldValue);
+  }
+
+  if (Array.isArray(oldValue)) {
+    return mapProperiesStates.modified(key, value, stringifyFunc(oldValue, depth));
+  }
+
+  return mapProperiesStates.modified(key, value, oldValue);
+};
+
+const stringify = (differences, depth) => {
   const stringifiedDiffs = differences
     .reduce((acc, diff) => {
-      const {
-        state,
-        key,
-        value,
-        prevValue,
-      } = diff;
+      const { state } = diff;
 
-      const result = mapProperiesStates[state](key, value, prevValue);
-
-      return [...acc, ...result];
+      if (state === 'modified') {
+        return [...acc, ...stringifyModifiedDiff(diff, depth + 3, stringify)];
+      }
+      return [...acc, ...stringifySimpleDiff(diff, depth + 3, stringify)];
     }, [])
-    .map((val) => `  ${val}`)
+    .map((val) => `${' '.repeat(depth)}${val}`)
     .join('\n');
 
-  return `{\n${stringifiedDiffs}\n}`;
+  return `{\n${stringifiedDiffs}\n${' '.repeat(depth - 1)}}`;
 };
 
-export default stringify;
+export default (differences) => stringify(differences, 1);
