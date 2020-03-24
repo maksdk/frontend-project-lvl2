c42eedee9500aa23b0b40907b2b84ec05fce36b0
maksdk <havryshchenko@gmail.com>
2020-03-24 08:19:32 +0200

    plain formatter

--- __tests__/formatters/plain.test.js
+++ __tests__/formatters/plain.test.js
@@ -1,67 +1,60 @@
 // @ts-check
+import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
+import uniqueString from 'unique-string';
 import findDiff from '../../src/findDiff';
 import formToPlain from '../../src/formatters/index';
 
 const getFixturePath = (filename) => path.join(__dirname, '../..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const before = {
-  one: 'one',
-  two: {
-    three: 'three',
-  },
-  five: {
-    six: 'six',
-  },
-  ten: 10,
-  twelve: 'twelve',
-  twenty: {
-    one: 'one',
-    two: 'two',
-    five: 'five',
-  },
-};
-
-const after = {
-  four: 'four',
-  seven: {
-    eight: 'eight',
-  },
-  five: {
-    six: 'not - six',
-  },
-  ten: 100,
-  twelve: {
-    eleven: 'eleven',
-  },
-  twenty: {
-    one: 'not - one',
-    two: 'two',
-    three: {},
-  },
-};
-
-// const recursiveMutableSort = (arr) => {
-//   arr.sort((a, b) => {
-//     if (a.children) recursiveMutableSort(a.children);
-//     if (b.children) recursiveMutableSort(b.children);
-//     if (a.key > b.key) return 1;
-//     if (a.key < b.key) return -1;
-//     return 0;
-//   });
-// };
-// recursiveMutableSort(differences);
-
-const differences = findDiff(before, after);
-
-
 describe('Plain formatter', () => {
-  it('', () => {
-    console.log(differences)
-    const actual = formToPlain(differences, 'plain');
-    const expected = readFile('formatters/result.txt');
-    expect(expected).toEqual(actual);
+  it('test 1', () => {
+    const before = {
+      unchanged: {
+        prop: 'prop',
+      },
+      one: 'one',
+      two: {
+        three: 'three',
+      },
+      five: {
+        six: 'six',
+      },
+      ten: 10,
+      twelve: 'twelve',
+      twenty: {
+        one: 'one',
+        two: 'two',
+        five: 'five',
+      },
+    };
+    const after = {
+      unchanged: {
+        prop: 'prop',
+      },
+      four: 'four',
+      seven: {
+        eight: 'eight',
+      },
+      five: {
+        six: 'not - six',
+      },
+      ten: 100,
+      twelve: {
+        eleven: 'eleven',
+      },
+      twenty: {
+        one: 'not - one',
+        two: 'two',
+        three: {},
+      },
+    };
+    const differences = findDiff(before, after);
+    const separator = uniqueString();
+    const expected = readFile('formatters/result.txt').replace(/(\r\n|\n|\r)/gm, separator).split(separator);
+    const actual = formToPlain(differences, 'plain').replace(/(\r\n|\n|\r)/gm, separator).split(separator);
+    expect(_.sortBy(expected)).toEqual(_.sortBy(actual));
   });
 });
