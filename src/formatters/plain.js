ff22daa1d11dcd7f23aa321fd9944b742029b9bb
maksdk <havryshchenko@gmail.com>
2020-03-24 08:20:02 +0200

    realized plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,17 +1,5 @@
 // @ts-check
 import _ from 'lodash';
-// Property 'timeout' was changed from 50 to 20
-// Property 'proxy' was deleted
-// Property 'common.setting4' was deleted
-// Property 'common.setting5' was deleted
-// Property 'common.setting2' was added with value: 200
-// Property 'common.setting6.ops' was added with value: 'vops'
-// Property 'common.sites' was added with value: 'hexlet.io'
-// Property 'group1.baz' was changed from 'bars' to 'bas'
-// Property 'group3' was deleted
-// Property 'verbose' was added with value: true
-// Property 'group2' was added with value: [complex value]
-
 
 const stringifyValue = (value) => {
   if (_.isObject(value)) return '[complex value]';
@@ -21,14 +9,12 @@ const stringifyValue = (value) => {
 
 const mapTexts = {
   changed: (key, newValue, oldValue) => (`Property '${key}' was changed from ${stringifyValue(oldValue)} to ${stringifyValue(newValue)}`),
-  modified: (key, newValue, oldValue) => (`Property '${key}' was changed from ${stringifyValue(oldValue)} to ${stringifyValue(newValue)}`),
   deleted: (key) => `Property '${key}' was deleted`,
   added: (key, newValue) => `Property '${key}' was added with value: ${stringifyValue(newValue)}`,
-  new: (key, newValue) => `Property '${key}' was added with value: ${stringifyValue(newValue)}`,
 };
 
-const removeUnchangedProperties = (diffs) => {
-  return diffs.reduce((acc, diff) => {
+const removeUnchangedProperties = (differences) => {
+  return differences.reduce((acc, diff) => {
     const { children, state } = diff;
 
     if (state === 'unchanged') {
@@ -43,133 +29,36 @@ const removeUnchangedProperties = (diffs) => {
   }, []);
 };
 
-const buildFullPaths = (diffs) => {
+const generateFlattenFullPaths = (differences) => {
   const iter = (diff, pathAcc, acc) => {
-    const { key, children, state } = diff;
+    const { key, children } = diff;
     const newPath = [...pathAcc, key];
 
     if (Array.isArray(children)) {
-      if (state === 'added') {
-        return [...acc, { ...diff, fullPath: newPath }];
-      }
-
-      if (state === 'deleted') {
-        return [...acc, { ...diff, fullPath: newPath }];
-      }
-
       return children.reduce((newAcc, newDiff) => iter(newDiff, newPath, newAcc), acc);
     }
 
     return [...acc, { ...diff, fullPath: newPath }];
   };
 
-  return diffs.reduce((acc, diff) => [...acc, ...iter(diff, [], [])], []);
+  return differences.reduce((acc, diff) => [...acc, ...iter(diff, [], [])], []);
 };
 
-const form = (diffs) => {
-  return diffs.map(diff => {
-    if (diff.state === 'changed') {
-      return { name: diff.fullPath.join('.'), state: diff.state, to: stringifyValue(diff.value), from: stringifyValue(diff.oldValue) };
-    }
-    return { name: diff.fullPath.join('.'), state: diff.state, value: stringifyValue(diff.value) };
-  });
+const stringifyDifferences = (differences) => {
+  return differences.map((diff) => {
+    const {
+      state,
+      value,
+      oldValue,
+      fullPath,
+    } = diff;
+    return mapTexts[state](fullPath.join('.'), value, oldValue);
+  }).join('\n');
 };
 
-// const form = (diffs) => {
-//   const iter = (diff, pathAcc, acc) => {
-//     const { key, children, state } = diff;
-
-//     // if (!mapTexts[state]) return acc;
-//     if (state === 'unchanged') return acc;
-
-//     const newPath = [...pathAcc, key];
-
-//     if (children) {
-//       return children.reduce((newAcc, newDiff) => iter(newDiff, newPath, newAcc), acc);
-//     }
-
-//     return [...acc, { path: newPath, state }];
-//   };
-
-//   return diffs.reduce((acc, diff) => [...acc, ...iter(diff, [], [])], []);
-// };
-
-
-const diffs = [
-  {
-    key: 'common',
-    state: 'changed',
-    // state: 'overlap',
-    children: [
-      { key: 'follow', value: false, state: 'added' },
-      { key: 'setting1', value: 'Value 1', state: 'unchanged' },
-      // { key: 'setting1', value: 'Value 1' },
-      { key: 'setting2', value: 200, state: 'deleted' },
-      {
-        key: 'setting3',
-        oldValue: true,
-        state: 'changed',
-        children: [
-          { key: 'key', value: 'value' }],
-      },
-      {
-        key: 'setting6',
-        state: 'changed',
-        children: [
-          { key: 'key', value: 'value', state: 'unchanged' },
-          // { key: 'key', value: 'value' },
-          { key: 'ops', value: 'vops', state: 'added' }],
-      },
-      { key: 'setting4', value: 'blah blah', state: 'added' },
-      {
-        key: 'setting5',
-        state: 'added',
-        children: [
-          { key: 'key5', value: 'value5' }],
-      },
-    ],
-  },
-  {
-    key: 'group1',
-    state: 'changed',
-    children: [
-      {
-        key: 'baz',
-        value: 'bars',
-        state: 'changed',
-        oldValue: 'bas',
-      },
-      { key: 'foo', value: 'bar', state: 'unchanged' },
-      // { key: 'foo', value: 'bar' },
-      {
-        key: 'nest',
-        state: 'changed',
-        value: 'str',
-        oldValue: [
-          { key: 'key', value: 'value' }],
-      }],
-  },
-  {
-    key: 'group2',
-    state: 'deleted',
-    children: [
-      { key: 'abc', value: 123456 }],
-  },
-  {
-    key: 'group3',
-    state: 'added',
-    children: [
-      { key: 'fee', value: 100500 }],
-  },
-];
-// const res = form(expected);
-// console.log(res)
-const res = removeUnchangedProperties(diffs);
-// console.log(res);
-// console.log(res[0]);
-const built = buildFullPaths(res);
-console.log(form(built))
-// export default (differences) => {
-//   const result = form(differences);
-//   console.log(result)
-// };
+export default (differences) => {
+  const clearedFromUnchanged = removeUnchangedProperties(differences);
+  const flattenFullPaths = generateFlattenFullPaths(clearedFromUnchanged);
+  const stringifiedDiffs = stringifyDifferences(flattenFullPaths);
+  return stringifiedDiffs;
+};
