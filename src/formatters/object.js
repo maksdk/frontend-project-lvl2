0c3e93af24bc9acf7de6ed9fbf23a9b5904f88be
maksdk <havryshchenko@gmail.com>
2020-03-25 12:59:24 +0200

    changed object formatter algoritm

--- src/formatters/object.js
+++ src/formatters/object.js
@@ -1,65 +1,78 @@
 // @ts-check
+import _ from 'lodash';
 
-const mapProperiesStates = {
-  new: (key, value) => [`+ ${key}: ${value}`],
-  modified: (key, value, prevVelue) => [`+ ${key}: ${value}`, `- ${key}: ${prevVelue}`],
-  deleted: (key, value) => [`- ${key}: ${value}`],
-  unchanged: (key, value) => [`  ${key}: ${value}`],
+const defaultMargin = ' '.repeat(3);
+
+const generateMargin = (depth, margin, prefix = '') => {
+  const fullMargin = margin.repeat(depth);
+  const splitedMargin = fullMargin.split('');
+  splitedMargin.splice(splitedMargin.length - prefix.length, prefix.length, prefix);
+  return splitedMargin.join('');
 };
 
-const stringifySimpleDiff = (diff, depth, stringifyFunc) => {
-  const {
-    state = 'unchanged',
-    key,
-    value,
-    children,
-  } = diff;
-
-  if (Array.isArray(children)) {
-    return mapProperiesStates[state](key, stringifyFunc(children, depth));
-  }
-
-  return mapProperiesStates[state](key, value);
+const wrapValues = (values, depth) => {
+  const joinedValues = values.join('\n');
+  return `{\n${joinedValues}\n${defaultMargin.repeat(depth - 1)}}`;
 };
 
-const stringifyModifiedDiff = (diff, depth, stringifyFunc) => {
-  const {
-    key,
-    value,
-    children,
-    oldValue,
-  } = diff;
+const stringifyValue = (value, depth = 0) => {
+  if (!_.isPlainObject(value)) return value;
 
-  if (Array.isArray(children) && Array.isArray(oldValue)) {
-    return mapProperiesStates
-      .modified(key, stringifyFunc(children, depth), stringifyFunc(oldValue, depth));
-  }
+  const values = Object.entries(value)
+    .map(([k, v]) => `${generateMargin(depth, defaultMargin, '')}${k}: ${stringifyValue(v, depth + 1)}`);
 
-  if (Array.isArray(children)) {
-    return mapProperiesStates.modified(key, stringifyFunc(children, depth), oldValue);
-  }
-
-  if (Array.isArray(oldValue)) {
-    return mapProperiesStates.modified(key, value, stringifyFunc(oldValue, depth));
-  }
-
-  return mapProperiesStates.modified(key, value, oldValue);
+  return wrapValues(values, depth);
 };
 
-const stringify = (differences, depth) => {
-  const stringifiedDiffs = differences
-    .reduce((acc, diff) => {
-      const { state } = diff;
+const stringifyDifferences = (differences, depth) => {
+  const values = differences.reduce((acc, diff) => {
+    const {
+      state,
+      children,
+      key,
+      value,
+      oldValue,
+    } = diff;
 
-      if (state === 'modified') {
-        return [...acc, ...stringifyModifiedDiff(diff, depth + 3, stringify)];
-      }
-      return [...acc, ...stringifySimpleDiff(diff, depth + 3, stringify)];
-    }, [])
-    .map((val) => `${' '.repeat(depth)}${val}`)
-    .join('\n');
+    if (children) {
+      const prefix = '  ';
+      const margin = generateMargin(depth, defaultMargin, prefix);
+      return [...acc, `${margin}${key}: ${stringifyDifferences(children, depth + 1)}`];
+    }
 
-  return `{\n${stringifiedDiffs}\n${' '.repeat(depth - 1)}}`;
+    if (state === 'added') {
+      const prefix = '+ ';
+      const margin = generateMargin(depth, defaultMargin, prefix);
+      return [...acc, `${margin}${key}: ${stringifyValue(value, depth + 1)}`];
+    }
+
+    if (state === 'deleted') {
+      const prefix = '- ';
+      const margin = generateMargin(depth, defaultMargin, prefix);
+      return [...acc, `${margin}${key}: ${stringifyValue(value, depth + 1)}`];
+    }
+
+    if (state === 'changed') {
+      const prefix1 = '+ ';
+      const prefix2 = '- ';
+      const margin1 = generateMargin(depth, defaultMargin, prefix1);
+      const margin2 = generateMargin(depth, defaultMargin, prefix2);
+      return [
+        ...acc,
+        `${margin1}${key}: ${stringifyValue(value, depth + 1)}`,
+        `${margin2}${key}: ${stringifyValue(oldValue, depth + 1)}`];
+    }
+
+    if (state === 'unchanged') {
+      const prefix = '  ';
+      const margin = generateMargin(depth, defaultMargin, prefix);
+      return [...acc, `${margin}${key}: ${stringifyValue(value, depth + 1)}`];
+    }
+
+    throw new Error(`Such state: ${state} is not registered!`);
+  }, []);
+
+  return wrapValues(values, depth);
 };
 
-export default (differences) => stringify(differences, 1);
+export default (differences) => stringifyDifferences(differences, 0);
