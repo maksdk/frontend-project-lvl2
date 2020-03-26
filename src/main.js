5ff93f1159ce210f1dc7677e53b97701226c7fd1
maksdk <havryshchenko@gmail.com>
2020-03-26 11:17:05 +0200

    separated functions calls

--- src/main.js
+++ src/main.js
@@ -18,8 +18,14 @@ export default (path1, path2, format) => {
   const extention1 = getExtension(path1);
   const extention2 = getExtension(path2);
 
-  const config1 = parse(readFile(fixPath(path1)), extention1);
-  const config2 = parse(readFile(fixPath(path2)), extention2);
+  const fixedPath1 = fixPath(path1);
+  const fixedPath2 = fixPath(path2);
+
+  const file1 = readFile(fixedPath1);
+  const file2 = readFile(fixedPath2);
+
+  const config1 = parse(file1, extention1);
+  const config2 = parse(file2, extention2);
 
   const differences = generateDifferences(config1, config2);
   const formedDifferences = formDifferences(differences, format);
