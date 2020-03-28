6313a65de15704b28e511f96d596a3a93542150f
maksdk <havryshchenko@gmail.com>
2020-03-28 09:00:15 +0200

    fixtures for parsers test

---
+++ __fixtures__/parsedConfigs/config1.js
@@ -0,0 +1,21 @@
+// @ts-check
+export default {
+  common: {
+    setting1: 'Value 1',
+    setting2: 200,
+    setting3: true,
+    setting6: {
+      key: 'value',
+    },
+  },
+  group1: {
+    baz: 'bas',
+    foo: 'bar',
+    nest: {
+      key: 'value',
+    },
+  },
+  group2: {
+    abc: 123456,
+  },
+};
