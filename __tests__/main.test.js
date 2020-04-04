72777a5039cc0a2b9be779a6c50b290d6e73fbe7
maksdk <havryshchenko@gmail.com>
2020-04-04 09:36:38 +0300

    removed actual-expected cleares

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -9,36 +9,6 @@ const fileFormats = ['json', 'yaml', 'ini'];
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const sortDifferencesMutable = (arr) => {
-  arr.sort((a, b) => {
-    if (a.children) sortDifferencesMutable(a.children);
-    if (b.children) sortDifferencesMutable(b.children);
-    if (a.key > b.key) return 1;
-    if (a.key < b.key) return -1;
-    return 0;
-  });
-};
-
-const mapOutputClears = {
-  object: (value) => (
-    value.replace(/(\r\n|\n|\r)/gm, '')
-      .split(' ')
-      .filter((v) => v !== '')
-      .sort()
-  ),
-  plain: (value) => {
-    const separator = '#%-Some-test-separator-%#';
-    return value.replace(/(\r\n|\n|\r)/gm, separator)
-      .split(separator)
-      .sort();
-  },
-  json: (value) => {
-    const parsedValue = JSON.parse(value);
-    sortDifferencesMutable(parsedValue);
-    return parsedValue;
-  },
-};
-
 describe('Check exceptions', () => {
   it('Wrong paths', () => {
     const p = getFixturePath('before.json');
@@ -71,19 +41,15 @@ describe('Check exceptions', () => {
 });
 
 describe('Check outputs', () => {
-  it('Test 1', () => {
-    fileFormats.forEach((fileFormat) => {
-      const p1 = getFixturePath(`before.${fileFormat}`);
-      const p2 = getFixturePath(`after.${fileFormat}`);
+  fileFormats.forEach((fileFormat) => {
+    const path1 = getFixturePath(`before.${fileFormat}`);
+    const path2 = getFixturePath(`after.${fileFormat}`);
 
-      outputFormats.forEach((outputFormat) => {
+    outputFormats.forEach((outputFormat) => {
+      it(`Config format: '${fileFormat}'. Output format: '${outputFormat}'`, () => {
         const expected = readFile(`outputs/${outputFormat}.txt`);
-        const actual = generate(p1, p2, outputFormat);
-
-        const clearedExpected = mapOutputClears[outputFormat](expected);
-        const clearedActual = mapOutputClears[outputFormat](actual);
-
-        expect(clearedActual).toEqual(clearedExpected);
+        const actual = generate(path1, path2, outputFormat);
+        expect(actual).toEqual(expected);
       });
     });
   });
