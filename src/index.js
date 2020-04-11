5c7da433576e75f9ef172d949bc3849faa0723ca
maksdk <havryshchenko@gmail.com>
2020-04-11 08:42:47 +0300

    correct names to improve readable

--- src/index.js
+++ src/index.js
@@ -5,25 +5,22 @@ import parse from './parsers';
 import generateDifferences from './generateDifferences';
 import formDifferences from './formatters';
 
-const getExtension = (filename) => {
-  const ext = path.extname(filename).split('.');
-  return ext[ext.length - 1];
-};
+const getExtension = (filename) => path.extname(filename).slice(1);
 
 const getFilePath = (filename) => path.resolve(process.cwd(), filename);
 const readFile = (filename) => fs.readFileSync(getFilePath(filename), 'utf-8');
 
 export default (path1, path2, format) => {
-  const file1 = readFile(path1);
-  const file2 = readFile(path2);
+  const config1 = readFile(path1);
+  const config2 = readFile(path2);
 
   const extention1 = getExtension(path1);
   const extention2 = getExtension(path2);
 
-  const config1 = parse(file1, extention1);
-  const config2 = parse(file2, extention2);
+  const parsedConfig1 = parse(config1, extention1);
+  const parsedConfig2 = parse(config2, extention2);
 
-  const differences = generateDifferences(config1, config2);
+  const differences = generateDifferences(parsedConfig1, parsedConfig2);
   const formedResult = formDifferences(differences, format);
 
   return formedResult;
