1f0874088d18f7e6ba323d4358f3030370f05eb3
maksdk <havryshchenko@gmail.com>
2020-04-11 09:26:52 +0300

    replaced 'dynamic dispatcher' to 'switch'

--- src/formatters/object.js
+++ src/formatters/object.js
@@ -4,14 +4,6 @@ import _ from 'lodash';
 const defaultMargin = ' '.repeat(4);
 const lineBreaker = '\n';
 
-const supportedTypes = ['added', 'deleted', 'changed', 'unchanged', 'complex'];
-
-const prefixes = {
-  added: '+ ',
-  deleted: '- ',
-  default: '  ',
-};
-
 const generateMargin = (depth, margin, prefix = '') => {
   const fullMargin = margin.repeat(depth);
   const splitedMargin = fullMargin.split('');
@@ -19,9 +11,9 @@ const generateMargin = (depth, margin, prefix = '') => {
   return splitedMargin.join('');
 };
 
-const wrapValues = (values, depth) => {
-  const joinedValues = values.join(lineBreaker);
-  return `{${lineBreaker}${joinedValues}${lineBreaker}${defaultMargin.repeat(depth - 1)}}`;
+const wrapStringifiedDiffs = (diffs, depth) => {
+  const joinedDiffs = diffs.join(lineBreaker);
+  return `{${lineBreaker}${joinedDiffs}${lineBreaker}${defaultMargin.repeat(depth - 1)}}`;
 };
 
 const stringifyValue = (value, depth = 0) => {
@@ -30,58 +22,72 @@ const stringifyValue = (value, depth = 0) => {
     const values = Object.entries(value)
       .map(([k, v]) => `${generateMargin(depth, defaultMargin, '')}${k}: ${stringifyValue(v, depth + 1)}`);
 
-    return wrapValues(values, depth);
+    return wrapStringifiedDiffs(values, depth);
   }
   return value;
 };
 
-const stringifiedGenerators = {
-  complex: (diff, depth, stringifyDiffs) => {
-    const { key, children } = diff;
-    const prefix = prefixes.default;
-    const margin = generateMargin(depth, defaultMargin, prefix);
-    return [
-      `${margin}${key}: ${stringifyDiffs(children, depth + 1)}`];
-  },
-
-  changed: (diff, depth) => {
-    const { key, value, oldValue } = diff;
-    const prefix1 = prefixes.added;
-    const prefix2 = prefixes.deleted;
-    const margin1 = generateMargin(depth, defaultMargin, prefix1);
-    const margin2 = generateMargin(depth, defaultMargin, prefix2);
-    return [
-      `${margin1}${key}: ${stringifyValue(value, depth + 1)}`,
-      `${margin2}${key}: ${stringifyValue(oldValue, depth + 1)}`];
-  },
-
-  default: (diff, depth) => {
-    const { type, key, value } = diff;
-    const prefix = prefixes[type] || prefixes.default;
-    const margin = generateMargin(depth, defaultMargin, prefix);
-    return [
-      `${margin}${key}: ${stringifyValue(value, depth + 1)}`];
-  },
+const getPrefixFromType = (type) => {
+  switch (type) {
+    case 'added':
+      return '+ ';
+    case 'deleted':
+      return '- ';
+    case 'complex':
+    case 'changed':
+    case 'unchanged':
+      return '  ';
+    default:
+      throw new Error(`Such type: ${type} is not supported!`);
+  }
 };
 
-const stringifyDifferences = (differences, depth) => {
-  const values = differences.reduce((acc, diff) => {
+const stringifyComplexDiff = (diff, depth, formDiffs) => {
+  const { key, children } = diff;
+  const prefix = getPrefixFromType('complex');
+  const margin = generateMargin(depth, defaultMargin, prefix);
+  return [
+    `${margin}${key}: ${formDiffs(children, depth + 1)}`];
+};
+
+const stringifyChangedDiff = (diff, depth) => {
+  const { key, value, oldValue } = diff;
+  const prefix1 = getPrefixFromType('added');
+  const prefix2 = getPrefixFromType('deleted');
+  const margin1 = generateMargin(depth, defaultMargin, prefix1);
+  const margin2 = generateMargin(depth, defaultMargin, prefix2);
+  return [
+    `${margin1}${key}: ${stringifyValue(value, depth + 1)}`,
+    `${margin2}${key}: ${stringifyValue(oldValue, depth + 1)}`];
+};
+
+const stringifyDiff = (diff, depth) => {
+  const { type, key, value } = diff;
+  const prefix = getPrefixFromType(type);
+  const margin = generateMargin(depth, defaultMargin, prefix);
+  return [
+    `${margin}${key}: ${stringifyValue(value, depth + 1)}`];
+};
+
+const formDiffs = (diffs, depth) => {
+  const stringifiedDiffs = diffs.map((diff) => {
     const { type } = diff;
 
-    if (!supportedTypes.includes(type)) {
-      throw new Error(`Such type: ${type} is not supported!`);
+    switch (type) {
+      case 'complex':
+        return stringifyComplexDiff(diff, depth, formDiffs);
+      case 'changed':
+        return stringifyChangedDiff(diff, depth);
+      case 'added':
+      case 'deleted':
+      case 'unchanged':
+        return stringifyDiff(diff, depth);
+      default:
+        throw new Error(`Such type: ${type} is not supported!`);
     }
+  }, []).flat();
 
-    const generate = stringifiedGenerators[type] || stringifiedGenerators.default;
-
-    return [...acc, ...generate(diff, depth, stringifyDifferences)];
-  }, []);
-
-  return wrapValues(values, depth);
+  return wrapStringifiedDiffs(stringifiedDiffs, depth);
 };
 
-export default (differences) => {
-  if (differences.length === 0) return '{}';
-
-  return stringifyDifferences(differences, 1);
-};
+export default (diffs) => formDiffs(diffs, 1);
