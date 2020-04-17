cb4732a3ba1fd1f4553970996f41ba88b502c08f
maksdk <havryshchenko@gmail.com>
2020-04-17 08:52:19 +0300

    correct variables names

--- src/formatters/object.js
+++ src/formatters/object.js
@@ -1,10 +1,10 @@
 // @ts-check
 import _ from 'lodash';
 
-const defaultMarginLeft = ' '.repeat(4);
+const defaultIndent = ' '.repeat(4);
 
-const generateMargin = (depth, prefix = '') => {
-  const fullMargin = defaultMarginLeft.repeat(depth);
+const generateIndent = (depth, prefix = '') => {
+  const fullMargin = defaultIndent.repeat(depth);
   const splitedMargin = fullMargin.split('');
   splitedMargin.splice(splitedMargin.length - prefix.length, prefix.length, prefix);
   return splitedMargin.join('');
@@ -12,21 +12,21 @@ const generateMargin = (depth, prefix = '') => {
 
 const wrapStringifiedDiffs = (diffs, depth) => {
   const joinedDiffs = diffs.join('\n');
-  return `{${'\n'}${joinedDiffs}${'\n'}${defaultMarginLeft.repeat(depth - 1)}}`;
+  return `{${'\n'}${joinedDiffs}${'\n'}${defaultIndent.repeat(depth - 1)}}`;
 };
 
 const stringifyValue = (value, depth = 0) => {
   if (_.isString(value)) return value;
   if (_.isPlainObject(value)) {
     const values = Object.entries(value)
-      .map(([k, v]) => `${generateMargin(depth, '')}${k}: ${stringifyValue(v, depth + 1)}`);
+      .map(([k, v]) => `${generateIndent(depth, '')}${k}: ${stringifyValue(v, depth + 1)}`);
 
     return wrapStringifiedDiffs(values, depth);
   }
   return value;
 };
 
-const formDiffs = (diffs, depth) => {
+const buildDiffs = (diffs, depth) => {
   const stringifiedDiffs = diffs.map((diff) => {
     const {
       type,
@@ -40,26 +40,26 @@ const formDiffs = (diffs, depth) => {
     switch (type) {
       case 'complex':
         return [
-          `${generateMargin(depth, '  ')}${key}: ${formDiffs(children, depth + 1)}`];
+          `${generateIndent(depth, '  ')}${key}: ${buildDiffs(children, depth + 1)}`];
       case 'changed':
         return [
-          `${generateMargin(depth, '+ ')}${key}: ${stringifyValue(newValue, depth + 1)}`,
-          `${generateMargin(depth, '- ')}${key}: ${stringifyValue(oldValue, depth + 1)}`];
+          `${generateIndent(depth, '+ ')}${key}: ${stringifyValue(newValue, depth + 1)}`,
+          `${generateIndent(depth, '- ')}${key}: ${stringifyValue(oldValue, depth + 1)}`];
       case 'added':
         return [
-          `${generateMargin(depth, '+ ')}${key}: ${stringifyValue(value, depth + 1)}`];
+          `${generateIndent(depth, '+ ')}${key}: ${stringifyValue(value, depth + 1)}`];
       case 'deleted':
         return [
-          `${generateMargin(depth, '- ')}${key}: ${stringifyValue(value, depth + 1)}`];
+          `${generateIndent(depth, '- ')}${key}: ${stringifyValue(value, depth + 1)}`];
       case 'unchanged':
         return [
-          `${generateMargin(depth, '  ')}${key}: ${stringifyValue(value, depth + 1)}`];
+          `${generateIndent(depth, '  ')}${key}: ${stringifyValue(value, depth + 1)}`];
       default:
         throw new Error(`Such type: ${type} is not supported!`);
     }
-  }, []).flat();
+  }).flat();
 
   return wrapStringifiedDiffs(stringifiedDiffs, depth);
 };
 
-export default (diffs) => formDiffs(diffs, 1);
+export default (diffs) => buildDiffs(diffs, 1);
